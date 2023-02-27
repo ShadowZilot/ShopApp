e61790428bd5bb925d09e2a2ced864fa6dd667e5
ShadowZilot <e.ponomaryow2017@yandex.ru>
2023-02-27 11:17:50 +0300

    Init commit

---
+++ app/src/main/java/com/egorponomarev/shop_adhi/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.egorponomarev.shop_adhi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
