86393b5fa415be07f6af6d39af50d88373cc27a5
ShadowZilot <e.ponomaryow2017@yandex.ru>
2023-03-08 04:58:17 +0300

    Setup bottom nav

--- app/src/main/java/com/egorponomarev/shop_adhi/MainActivity.kt
+++ app/src/main/java/com/egorponomarev/shop_adhi/MainActivity.kt
@@ -2,10 +2,34 @@ package com.egorponomarev.shop_adhi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.mainHostNav) as NavHostFragment
+        val navController = navHostFragment.navController
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)
+        bottomNav.setupWithNavController(navController)
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.actionHome -> {
+                    navController.navigate(R.id.toStoreFragment)
+                    true
+                }
+                R.id.actionProfile -> {
+                    navController.navigate(R.id.toUserProfile)
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
