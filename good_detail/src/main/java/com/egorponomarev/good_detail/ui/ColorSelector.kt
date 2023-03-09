ece6aa841f41dfad27062032953b3bf8f1d6728d
ShadowZilot <e.ponomaryow2017@yandex.ru>
2023-03-09 03:13:15 +0300

    Implemented color selector

--- good_detail/src/main/java/com/egorponomarev/good_detail/ui/ColorSelector.kt
+++ good_detail/src/main/java/com/egorponomarev/good_detail/ui/ColorSelector.kt
@@ -1,6 +1,14 @@
 package com.egorponomarev.good_detail.ui
 
+import android.content.res.ColorStateList
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.GradientDrawable
+import android.util.TypedValue
+import android.view.View
+import android.view.ViewGroup
 import android.widget.LinearLayout
+import androidx.core.graphics.toColorInt
+import androidx.core.view.marginStart
 
 /**
  * Human Developing Soft
@@ -14,9 +22,52 @@ interface ColorSelector {
     class Base(
         private val mContainer: LinearLayout
     ) : ColorSelector {
+        private val mItems = mutableListOf<View>()
+        private val mDrawables = mutableListOf<GradientDrawable>()
+        private var mCurrentColor = 0
 
         override fun setup(colors: List<String>) {
+            for (i in colors.indices) {
+                mDrawables.add(
+                    GradientDrawable().apply {
+                        color = ColorStateList.valueOf(colors[i].toColorInt())
+                        cornerRadius = 9f
+                    }
+                )
+                mItems.add(
+                    View(mContainer.context).apply {
+                        background = mDrawables[i]
+                        elevation = 10f
+                        setOnClickListener {
+                            onSelectColor(i)
+                        }
+                        layoutParams = LinearLayout.LayoutParams(
+                            TypedValue.applyDimension(
+                                TypedValue.COMPLEX_UNIT_DIP,
+                                34f, mContainer.resources.displayMetrics
+                            ).toInt(),
+                            TypedValue.applyDimension(
+                                TypedValue.COMPLEX_UNIT_DIP,
+                                26f, mContainer.resources.displayMetrics
+                            ).toInt(),
+                        ).apply {
+                            this.marginEnd = TypedValue.applyDimension(
+                                TypedValue.COMPLEX_UNIT_DIP,
+                                14f, mContainer.resources.displayMetrics
+                            ).toInt()
+                        }
+                    }
+                )
+                mContainer.addView(mItems[i])
+            }
+        }
 
+        private fun onSelectColor(index: Int) {
+            mDrawables[mCurrentColor].setStroke(0, "#ADADAD".toColorInt())
+            mItems[mCurrentColor].invalidate()
+            mCurrentColor = index
+            mDrawables[mCurrentColor].setStroke(5, "#ADADAD".toColorInt())
+            mItems[mCurrentColor].invalidate()
         }
     }
 }
